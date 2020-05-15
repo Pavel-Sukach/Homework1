64f0be07a153f02f42ec04f4e2aa931dd37d79ff
Pavel-Sukach <pashnya87@gmail.com>
2020-05-15 18:34:25 +0300

    5th homework with correction

--- src/homework5/Task0/Program.java
+++ src/homework5/Task0/Program.java
@@ -3,22 +3,11 @@ package homework5.Task0;
 public class Program {
     public static void main(String[] args) {
         Shape round = new Round(8);
+        Shape triangular = new Triangular(2, 20);
+        Shape rectangular = new Rectangular(3, 12);
         try {
             round.square();
-        }
-        catch (SquareIsTooLargeException e){
-            e.squareIsTooLarge();
-        }
-
-        Shape triangular = new Triangular(15, 20);
-        try {
             triangular.square();
-        } catch (SquareIsTooLargeException e) {
-            e.squareIsTooLarge();
-        }
-
-        Shape rectangular = new Rectangular(11, 12);
-        try {
             rectangular.square();
         } catch (SquareIsTooLargeException e) {
             e.squareIsTooLarge();
